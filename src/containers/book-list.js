32f307a1f6cfef70ba2c05fda7df020091b089be
Aniruddh Gopale <anigopale@gmail.com>
2017-10-01 19:17:26 +0530

    book-list container created

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
-  rendreList(){
+class BookList extends Component {
+  renderList(){
     return this.props.books.map((books) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li key={books.title} className="list-group-item">{books.title}</li>
       );
     });
-  }//for every props.books, arrow function runs taking books as an argument, returns a jsx list element for every book title
+  }//for every props.books, arrow function runs taking books as an argument, returns a jsx list element for every books.title
 
   render() {
     return (
-      <ul className="list-group col-sm-4">
+      <ul className="listgroup col-sm-4">
         {this.renderList()}
       </ul>
     )
   }// renders jsx returned from renderList(), see relderList()
 }
+
+function mapStateToProps(state) {
+  return {
+    books:state.books
+  }
+}
+ //maps state to props of the container
+ // container re-renders if appstate changes
+ // the returned object must contain the required props as keys
+
+ export default connect(mapStateToProps)(BookList);
+ // connects returned props to component BookList
+ // this is exported instead of plain component
