3eef469e07cb268bcc98903aecfd40bf25f368e7
Aniruddh Gopale <anigopale@gmail.com>
2017-10-01 22:03:26 +0530

    an action created

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log('a book has been selected ', book.title);
-}
+  // selectBook is an actioncreator, it returns an actions
+  // object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
+}//selectBook is a props of the container BookList
